9cd9bb1756a80b1dd24dda8d53f27dcd4258b76a
yifanx98 <yifan.x98@gmail.com>
2024-03-26 20:50:42 +1100

    add function in option pricer, use BSM model to calculate call and put price

--- optionStrategy/main.py
+++ optionStrategy/main.py
@@ -4,3 +4,9 @@ from expiration_management import ExpirationManagement
 from margin_management import MarginManagement
 from strategy_selection import StrategySelection
 from continuous_monitoring import ContinuousMonitoring
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- optionStrategy/option_pricer.py
+++ optionStrategy/option_pricer.py
@@ -1,8 +1,29 @@
-class OptionPricer:
-    def __init__(self, stock_data, risk_free_rate):
-        self.stock_data = stock_data
-        self.risk_free_rate = risk_free_rate
+import math
+from scipy.stats import norm
 
-    def estimate_option_price(self, strike_price, expiration_date):
-        # Implement option pricing model
-        pass
+class OptionPricer:
+    def __init__(self, stock_price, risk_free_rate, volatility):
+        self.stock_price = stock_price
+        self.risk_free_rate = risk_free_rate
+        self.volatility = volatility
+
+    def calculate_d1(self, strike_price, expiration_date):
+        return (math.log(self.stock_price / strike_price) + (
+                    self.risk_free_rate + 0.5 * self.volatility ** 2) * expiration_date) / (
+                         self.volatility * math.sqrt(expiration_date))
+
+    def estimate_call_price(self, strike_price, expiration_date):
+        # Implement BSM pricing model for call option
+        d1 = self.calculate_d1(strike_price, expiration_date)
+        d2 = d1 - self.volatility * math.sqrt(expiration_date)
+        call_price = self.stock_price * norm.cdf(d1) - strike_price * math.exp(
+            - self.risk_free_rate * expiration_date) * norm.cdf(d2)
+        return call_price
+
+    def estimate_put_price(self, strike_price, expiration_date):
+        # Implement BSM pricing model for put option
+        d1 = self.calculate_d1(strike_price, expiration_date)
+        d2 = d1 - self.volatility * math.sqrt(expiration_date)
+        put_price = strike_price * math.exp(-self.risk_free_rate * expiration_date) * norm.cdf(
+            -d2) - self.stock_price * norm.cdf(-d1)
+        return put_price
