639b9a21f02121f61f1b7e47521b50989581661c
yifanx98 <yifan.x98@gmail.com>
2024-04-01 16:26:37 +1100

    Add option_pricing functions (BSM), include test_option_pricing

--- optionStrategy/option_pricing.py
+++ optionStrategy/option_pricing.py
@@ -1,13 +1,49 @@
+import datetime
+from scipy.stats import norm
+import math
+
 from optionStrategy.option import Option
 from optionStrategy.stock import Stock
+import optionStrategy.util as util
+
+"""
+volatility: annum
+risk free rate: continuous
+"""
 
 
-class OptionPricing:
-    """
-    volatility: daily
-    risk free rate: continuous
-    """
+def bsm_pricing(stock: Stock, option: Option, risk_free_rate):
+    time_to_maturity = (option.expiration_date - stock.current_price.timeStamp) / datetime.timedelta(days=365)
+    volatility = util.daily_vol_to_annual(stock.volatility)
+    if option.option_type == "Call":
+        return calculate_call_price(stock.current_price.price, option.strike_price.price,
+                                    volatility, time_to_maturity, risk_free_rate)
+    elif option.option_type == "Put":
+        return calculate_put_price(stock.current_price.price, option.strike_price.price,
+                                   volatility, time_to_maturity, risk_free_rate)
+    else:
+        raise ValueError("Invalid option type")
 
-    def pricing(stock: Stock, option: Option, risk_free_rate):
-        # Implement BSM model to price option
-        pass
+
+def calculate_call_price(stock_price, strike_price, volatility, time_to_maturity, risk_free_rate):
+    d1 = calculate_d1(stock_price, volatility, strike_price, time_to_maturity, risk_free_rate)
+    d2 = calculate_d2(d1, volatility, time_to_maturity)
+    call_price = stock_price * norm.cdf(d1) - strike_price * math.exp(-risk_free_rate * time_to_maturity) * norm.cdf(d2)
+    return call_price
+
+
+def calculate_put_price(stock_price, strike_price, volatility, time_to_maturity, risk_free_rate):
+    d1 = calculate_d1(stock_price, volatility, strike_price, time_to_maturity, risk_free_rate)
+    d2 = calculate_d2(d1, volatility, time_to_maturity)
+    put_price = strike_price * math.exp(-risk_free_rate * time_to_maturity) * norm.cdf(-d2) - stock_price * norm.cdf(-d1)
+    return put_price
+
+
+def calculate_d1(stock_price, volatility, strike_price, time_to_maturity, risk_free_rate):
+    return (math.log(stock_price / strike_price) + (
+                risk_free_rate + 0.5 * volatility ** 2) * time_to_maturity) / (
+                     volatility * math.sqrt(time_to_maturity))
+
+
+def calculate_d2(d1, volatility, time_to_maturity):
+    return d1 - volatility * math.sqrt(time_to_maturity)
--- optionStrategy/util.py
+++ optionStrategy/util.py
@@ -1,1 +1,5 @@
+import math
 
+
+def daily_vol_to_annual(vol: float):
+    return vol * math.sqrt(252)
