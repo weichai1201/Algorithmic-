c9ed5061ffd87ee16b3920dcf79167a803b2aa4d
McNagi <huanjie@protonmail.com>
2024-04-03 01:44:35 +1100

    fix instance of option type

--- src/trading_strategies/financial_asset/option.py
+++ src/trading_strategies/financial_asset/option.py
@@ -15,18 +15,12 @@ class Option:
 
 
 class CallOption(Option):
-    def __init__(self, stock_symbol: Symbol, strike_price: Price, expiration_date: datetime, premium: Price):
-        super().__init__(stock_symbol, strike_price, expiration_date, premium)
-        self.option_type = "Call"
 
     def option_payoff(self, stock: Stock):
         return np.maximum(stock.current_price.price - self.strike_price.price, 0)
 
 
 class PutOption(Option):
-    def __init__(self, stock_symbol: Symbol, strike_price: Price, expiration_date: datetime, premium: Price):
-        super().__init__(stock_symbol, strike_price, expiration_date, premium)
-        self.option_type = "Put"
 
     def option_payoff(self, stock: Stock):
         payoff = np.maximum(self.strike_price.price - stock.current_price.price, 0)
--- src/trading_strategies/option_pricing.py
+++ src/trading_strategies/option_pricing.py
@@ -2,7 +2,7 @@ import datetime
 from scipy.stats import norm
 import math
 
-from src.trading_strategies.financial_asset.option import Option
+from src.trading_strategies.financial_asset.option import Option, CallOption, PutOption
 from src.trading_strategies.financial_asset.stock import Stock
 import src.util.util as util
 
@@ -15,10 +15,10 @@ import src.util.util as util
 def bsm_pricing(stock: Stock, option: Option, risk_free_rate):
     time_to_maturity = (option.expiration_date - stock.current_price.timeStamp) / datetime.timedelta(days=365)
     volatility = util.daily_vol_to_annual(stock.volatility)
-    if option.option_type == "Call":
+    if isinstance(option, CallOption):
         return calculate_call_price(stock.current_price.price, option.strike_price.price,
                                     volatility, time_to_maturity, risk_free_rate)
-    elif option.option_type == "Put":
+    elif isinstance(option, PutOption):
         return calculate_put_price(stock.current_price.price, option.strike_price.price,
                                    volatility, time_to_maturity, risk_free_rate)
     else:
