dc6a7fea5a2ff2be8603564ade6c83e2ece617c8
McNagi <huanjie@protonmail.com>
2024-04-02 23:37:23 +1100

    implement symbol

--- src/optionStrategy/financial_asset/symbol.py
+++ src/optionStrategy/financial_asset/symbol.py
@@ -1,3 +1,13 @@
-class StockSymbol:
+
+
+class Symbol:
     def __init__(self, symbol: str):
         self.symbol = symbol
+
+    def get_symbol(self) -> str:
+        return self.symbol
+
+    def __eq__(self, other):
+        if not isinstance(other, Symbol):
+            return False
+        return self.symbol.__eq__(other.get_symbol())
