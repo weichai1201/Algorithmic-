c359737a5a42ddd9c3998652491ba342504f2bbe
McNagi <huanjie@protonmail.com>
2024-04-04 08:53:30 +1100

    add transaction class

--- src/trading_strategies/transactions/transaction.py
+++ src/trading_strategies/transactions/transaction.py
@@ -1,2 +1,16 @@
+from datetime import datetime
+
+from src.trading_strategies.transactions.positions import Positions
+
+
 class Transaction:
-    pass
+
+    def __init__(self, positions: Positions, time: datetime):
+        self.__positions = positions
+        self.__time = time
+
+    def get_time(self):
+        return self.__time
+
+    def calculate_profit(self, other: "Transaction"):
+        pass
