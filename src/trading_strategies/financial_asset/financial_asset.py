31ed9facec38757351f76db09dc3e83508a12ac1
McNagi <huanjie@protonmail.com>
2024-04-08 19:23:24 +1000

    update interface of strategies

---
+++ src/trading_strategies/financial_asset/financial_asset.py
@@ -0,0 +1,8 @@
+from src.trading_strategies.financial_asset.price import Price
+from src.trading_strategies.financial_asset.symbol import Symbol
+
+
+class FinancialAsset:
+    def __init__(self):
+        pass
+
