ce5d34a01f5f910da59a685b714553b4fd136c14
McNagi <huanjie@protonmail.com>
2024-04-09 21:32:49 +1000

    add option strategy methods

--- src/trading_strategies/option_strategy/option_strategy.py
+++ src/trading_strategies/option_strategy/option_strategy.py
@@ -8,9 +8,11 @@ from src.trading_strategies.option_strategy.spec import Spec
 from src.trading_strategies.strategy.strategy_id import StrategyId
 from src.trading_strategies.transactions.positions import ShortPositions
 from src.trading_strategies.transactions.transaction import Transaction
+from src.util import exception
 
 
 class OptionStrategy:
+
     @abstractmethod
     def __init__(self, id: StrategyId, options: [Option], specs: [Spec], scale=1):
         self._id = id
@@ -18,7 +20,9 @@ class OptionStrategy:
         self._specs = specs
         self._scale = scale
         self._transactions = []
-        self._current_profit = 0
+        self._profits = []  # realised profits for each transactions
+        self._current_profit = 0  # reset when option expired and renewed, updated with stock price
+        self._cumulative_profit = 0  # updated after positions closed (or option expired)
 
     @abstractmethod
     def update(self, new_data):
@@ -46,18 +50,38 @@ class OptionStrategy:
             return Symbol("")
         return self._options[0].get_symbol()
 
+    def _peek_transaction(self):
+        """
+        :return: Last transaction if any. Catch index-out-of-bound but do nothing.
+        """
+        index = len(self._transactions) - 1
+        if index < 0:
+            exception.ExceptionHandler.raise_index_out_of_range("transactions", index)
+        return self._transactions[index]
+
+    @staticmethod
+    def _calculate_profit(premium: float, stock_price: float, strike_price: float, quantity) -> float:
+        payoff = max(0.0, strike_price - stock_price)
+        profit = (premium + payoff * 100) * quantity  # each option respond to 100 shares
+        return profit
+
 
 class NakedPut(OptionStrategy):
     def __init__(self, strategy_id: StrategyId, options: [PutOption], specs: [Spec], scale=1):
         super().__init__(strategy_id, options, specs, scale)
 
     def __short_put(self):
-        transaction = Transaction(ShortPositions(self._scale), self._options[0], datetime.datetime.now())
+        """
+        assume it is rolling again
+        """
+        option = self._options[0]
+        quantity = self._scale
+        transaction = Transaction(ShortPositions(quantity), option, datetime.datetime.now())
         # may require setting timezone as trading in the u.s. while user in au
         self._transactions.append(transaction)
-        pass
+        self._current_profit = option.get_premium() * quantity
 
-    def update(self, new_option: Option):
+    def update(self, new_option: FinancialAsset):
         pass
 
     def add_transaction(self, transaction: Transaction):
