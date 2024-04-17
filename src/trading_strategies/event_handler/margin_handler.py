5a0364bdcc02324eec3e0178c6538ed6695fdfec
yifanx98 <yifan.x98@gmail.com>
2024-04-17 20:05:07 +1000

    modify margin calculation for equity options (per contract margin)

--- src/trading_strategies/event_handler/margin_handler.py
+++ src/trading_strategies/event_handler/margin_handler.py
@@ -10,28 +10,28 @@ class MarginHandler:
         pass
 
     """
-    Equity Options
+    Equity Options (per contract margin)
     """
     def naked_call_margin(self, underlying_value, strike_price, premium):
         otm_amount = max(0, strike_price - underlying_value)
         margin1 = 0.2 * underlying_value - otm_amount + premium
         margin2 = 0.1 * underlying_value + premium
-        margin3 = 100
-        return max(margin1, margin2, margin3)
+        margin3 = 1
+        return max(margin1, margin2, margin3) * 100
 
-    # TODO: need to confirm about contract_value
-    def naked_put_margin(self, underlying_value, strike_price, premium, contract_value):
+    def naked_put_margin(self, underlying_value, strike_price, premium):
         otm_amount = max(0, underlying_value - strike_price)
         margin1 = 0.2 * underlying_value - otm_amount + premium
-        margin2 = 0.1 * contract_value + premium
-        margin3 = 100
-        return max(margin1, margin2, margin3)
+        margin2 = 0.1 * strike_price + premium
+        margin3 = 1
+        return max(margin1, margin2, margin3) * 100
 
     def straddle_margin(self, call_underlying_value, call_strike_price, call_premium, put_underlying_value,
-                              put_strike_price, put_premium, put_contract_value):
+                              put_strike_price, put_premium):
         naked_call_margin = self.naked_call_margin(call_underlying_value, call_strike_price, call_premium)
-        naked_put_margin = self.naked_put_margin(put_underlying_value, put_strike_price, put_premium,
-                                            put_contract_value)
+        naked_put_margin = self.naked_put_margin(put_underlying_value, put_strike_price, put_premium)
         return max(naked_call_margin, naked_put_margin) + put_premium if (naked_call_margin > naked_put_margin) else call_premium
 
+    def spread_margin(self, strike_long, strike_short):
+        return abs(strike_long - strike_short) * 100
 
