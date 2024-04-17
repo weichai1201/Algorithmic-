4bf122d7b50976ac6bba3fa5f0ea421b1eb815aa
McNagi <huanjie@protonmail.com>
2024-04-18 01:47:11 +1000

    update skeleton of da

---
+++ src/data_access/data_access.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+from src.trading_strategies.financial_asset.stock import Stock
+from src.trading_strategies.financial_asset.symbol import Symbol
+
+
+def request_historical_price(symbol: Symbol, date: datetime) -> Stock:
+    pass
