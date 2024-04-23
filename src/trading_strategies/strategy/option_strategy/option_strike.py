ad4f45a1885f005d3cc00b0212b13ffa3f4f1eea
yifanx98 <yifan.x98@gmail.com>
2024-04-23 12:03:34 +1000

    calculate strike price based on strike gap & no. of strikes

---
+++ src/trading_strategies/strategy/option_strategy/option_strike.py
@@ -0,0 +1,27 @@
+strike_gaps = [
+    (0, 0.05),
+    (2, 0.10),
+    (5, 0.25),
+    (15, 0.50),
+    (50, 1.00),
+    (100, 2.00)
+]
+
+
+def get_strike_gap(stock_price):
+    strike_gap = 0
+    for price, gap in strike_gaps:
+        if stock_price >= price:
+            strike_gap = gap
+        else:
+            return strike_gap
+
+
+def calculate_strike(stock_price, itm_otm, num_strikes):
+    strike_gap = get_strike_gap(stock_price)
+    if itm_otm:
+        strike = (stock_price / strike_gap + 1 - num_strikes) * strike_gap # otm
+    else:
+        strike = (stock_price / strike_gap + num_strikes) * strike_gap
+
+    return strike
