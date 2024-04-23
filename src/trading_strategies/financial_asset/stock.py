928515a0d76285163c93ea2a8bd8f308f0036085
yifanx98 <yifan.x98@gmail.com>
2024-04-23 11:36:29 +1000

    add get historical prices, remove list of prices in stock. add long run garch volatility

--- src/trading_strategies/financial_asset/stock.py
+++ src/trading_strategies/financial_asset/stock.py
@@ -1,18 +1,24 @@
 import numpy as np
 import statistics
+from arch import arch_model
 
 from src.trading_strategies.financial_asset.financial_asset import FinancialAsset
 from src.trading_strategies.financial_asset.price import Price
 from src.trading_strategies.financial_asset.symbol import Symbol
+from src.util.read_file import get_historical_values
 
+
+# file_path = '../src/data/sp500_adj_close_prices.csv'
+file_path = '/Users/yifanxiao/Desktop/csl.csv'
 
 class Stock (FinancialAsset):
-    def __init__(self, symbol: Symbol, current_price: Price, historical_price=list[Price]):
+    def __init__(self, symbol: Symbol, current_price: Price):
         super().__init__()
         self._symbol = symbol
         self.current_price = current_price
-        self.historical_price = historical_price
+        # self.historical_price = historical_price
         self.volatility = self.calculate_volatility()
+        self.garch_long_run = self.get_garch()
 
     def calculate_volatility(self):
         returns = self.get_returns()
@@ -24,9 +30,20 @@ class Stock (FinancialAsset):
         return sum(returns) / len(returns)
 
     def get_returns(self):
-        prices = [price.get_price() for price in self.historical_price]
+        prices = [price for price in self.get_prices()]
         returns = np.diff(prices) / prices[:-1]
         return returns
+
+    def get_prices(self):
+        return get_historical_values(self.symbol, file_path, '2004-01-01', '2024-03-31')
+
+    def get_garch(self):
+        returns = self.get_returns()
+        model = arch_model(returns, vol='GARCH', p=1, q=1)
+        fit = model.fit()
+        vol = np.sqrt(fit.forecast(horizon=1000).variance).mean(axis=1)
+        return vol * np.sqrt(252)
+
 
     @property
     def symbol(self):
--- src/util/read_file.py
+++ src/util/read_file.py
@@ -2,6 +2,8 @@ import json
 import os
 import pandas as pd
 
+from src.trading_strategies.financial_asset.symbol import Symbol
+
 
 def read_file(file_path):
     if get_file_type(file_path).lower() == 'csv':
@@ -25,3 +27,9 @@ def read_json(file_path):
 def get_file_type(file_path):
     _, file_extension = os.path.splitext(file_path)
     return file_extension[1:]
+
+
+def get_historical_values(symbol: Symbol, file_path, start_date='2004-01-01', end_date='2024-01-01'):
+    data = read_file(file_path)
+    filtered_data = data[(data['Date'] <= end_date) & (data['Date'] >= start_date)]
+    return filtered_data[symbol.symbol]
