a41dbb4296a7e386c17859cad4b4837c1aea7397
McNagi <huanjie@protonmail.com>
2024-05-10 00:49:56 +1000

    implement simulated market

--- src/market/simulated_market.py
+++ src/market/simulated_market.py
@@ -1,8 +1,20 @@
 from src.market.order import Order
 
 
-class SimulatedMarket:
+class MarketSingletonMeta(type):
+    _instances = {}
 
-    @staticmethod
-    def submit_order(order: Order):
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class SimulatedMarket(MetaClass=MarketSingletonMeta):
+
+    def __init__(self):
+        self._order_book = []
+
+    def submit_order(self, order: Order):
         order.accept_order()
