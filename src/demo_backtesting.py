9ba546a223e103b7b1578c8de697f720024cbd09
McNagi <huanjie@protonmail.com>
2024-05-10 20:46:22 +1000

    add more backtesting run settings

--- src/demo_backtesting.py
+++ src/demo_backtesting.py
@@ -11,39 +11,56 @@ import matplotlib.pyplot as plt
 import ast
 
 
-# (0.010615660972065292, 'KO')(0.010711019177999836, 'JNJ')(0.010799562059302475, 'MCD')
-
+# volatility
+# (0.010615660972065288, 'KO') (0.010711019177999836, 'JNJ') (0.010799562059302473, 'MCD')
+# (0.04465957542892037, 'SMCI') (0.04234928634216114, 'ENPH') (0.04224288734882527, 'EPAM')
 def main():
-    # output directory
     foldername = "backtesting_result"
 
-    start_date = datetime(2005, 1, 1)
-    end_date = datetime(2010, 1, 1)
-    symbol_strs = ["SMCI", "KO", "AAPL", "CMA", "RHI"]
-    symbol_low_vol = ["KO", "JNJ", "MCD"]
+    start_date = datetime(2008, 1, 1)
+    end_date = datetime(2022, 1, 1)
 
+    low_vol = ["KO", "JNJ", "MCD"]
+    high_vol = ["SMCI", "ENPH", "EPAM"]
+    high_market_cap = ["MSFT", "AAPL", "NVDA", "GOOG", "AMZN"]
+    low_market_cap = ["BEN", "NCLH", "IVZ"]  # https://www.slickcharts.com/sp500
+    symbols = low_vol + high_vol + high_market_cap + low_market_cap
+    _run(symbols, start_date, end_date, foldername)
+    _run(symbols, start_date, end_date, foldername, is_itm=False)
+    _run(symbols, start_date, end_date, foldername, is_weekly=False)
+    _run(symbols, start_date, end_date, foldername, is_itm=False, is_weekly=False)
+    _run(symbols, start_date, end_date, foldername, num_of_strikes=2)
+
+
+def _run(symbols: [str], start_date, end_date, foldername, is_itm=True, is_weekly=True, num_of_strikes=1,
+         weekday="FRI"):
+    # output directory
     # option specification
-    is_itm = True
-    is_weekly = True
-    weekday = "FRI"
-    num_strike = 1
+    sub_folder = ""
+    if is_itm:
+        sub_folder += "itm_"
+    else:
+        sub_folder += "otm_"
+    if is_weekly:
+        sub_folder += "weekly_"
+    else:
+        sub_folder += "monthly_"
+    sub_folder += "num-strikes-" + str(num_of_strikes)
 
     strategies = dict()
-    for s in ["AAPL"]:
+    for s in symbols:
         strategy_id = StrategyId("NAKED_PUT_" + s)
-        strategy = RollingShortPut(strategy_id, Symbol(s), is_itm, is_weekly, weekday, num_strike)
+        strategy = RollingShortPut(strategy_id, Symbol(s), is_itm, is_weekly, weekday, num_of_strikes)
         strategies[strategy_id] = strategy
 
     backtester = run_daily_market_replay(strategies, start_date, end_date)
 
-
-
-# write to csv
+    # write to csv
     if not os.path.exists(foldername):
         os.makedirs(foldername)
     data = backtester.get_data()
     for strategy_id, df in data.items():
-        filename = f"{foldername}/{strategy_id.get_id()}"
+        filename = f"{foldername}/{sub_folder}/{strategy_id.get_id()}"
         df.to_csv(filename + ".csv")
 
         df['Cumulative'] = df['Cumulative'].apply(list)
@@ -68,6 +85,7 @@ def _plot(x, y, title="", filename=""):
         plt.savefig(filename)
     else:
         plt.show()
+    plt.clf()
 
 
 if __name__ == "__main__":
