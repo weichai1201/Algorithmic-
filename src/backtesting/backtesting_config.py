ce83f1085b0917b6904fa408046cd0de669f47a6
McNagi <huanjie@protonmail.com>
2024-05-12 01:52:50 +1000

    update backtesting config constructor

--- src/backtesting/backtesting_config.py
+++ src/backtesting/backtesting_config.py
@@ -1,17 +1,19 @@
 from datetime import datetime
+from typing import Callable
 
 
 class BacktestingConfig:
-    def __init__(self, start_date: datetime, end_date: datetime):
+    def __init__(self, strategy: Callable, start_date: datetime, end_date: datetime):
+        self.strategy = strategy
         self.start_date = start_date
         self.end_date = end_date
 
 
 class OptionBacktestingConfig(BacktestingConfig):
 
-    def __init__(self, start_date=datetime(year=2006, month=1, day=1), end_date=datetime(year=2022, month=1, day=1),
+    def __init__(self, strategy: Callable, start_date=datetime(year=2006, month=1, day=1), end_date=datetime(year=2022, month=1, day=1),
                  is_itm=True, is_weekly=True, num_of_strikes=1, weekday="FRI"):
-        super().__init__(start_date, end_date)
+        super().__init__(strategy, start_date, end_date)
         self.is_itm = is_itm
         self.is_weekly = is_weekly
         self.num_of_strikes = num_of_strikes
@@ -19,6 +21,6 @@ class OptionBacktestingConfig(BacktestingConfig):
 
 
 class ShortBacktestingConfig(OptionBacktestingConfig):
-    def __init__(self, start_date=datetime(year=2008, month=1, day=1),
+    def __init__(self, strategy: Callable,  start_date=datetime(year=2008, month=1, day=1),
                  end_date=datetime(year=2010, month=1, day=1)):
-        super().__init__(start_date, end_date)
+        super().__init__(strategy, start_date, end_date)
