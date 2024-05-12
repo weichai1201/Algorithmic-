dc72eefb462e874409e30f3c786767f790829c79
McNagi <huanjie@protonmail.com>
2024-05-12 13:02:06 +1000

    fix method call

--- src/market/simulated_market.py
+++ src/market/simulated_market.py
@@ -53,6 +53,6 @@ class SimulatedMarket(metaclass=MarketSingletonMeta):
     def _simulate_option_price(symbol: Symbol, stock_price: float, target_strike: float,
                                date: datetime, expiry: datetime, is_put=True):
         strike_price = get_closest_strike(stock_price, target_strike)
-        rf = DataAccess().get_risk_free(RatePeriod.TEN_YEAR, date)
+        rf = DataAccess().get_risk_free(date, RatePeriod.TEN_YEAR)
         premium = bsm_pricing2(Stock(symbol, Price(stock_price, date)), strike_price, expiry, [], rf.value, is_put)
         return strike_price, premium
