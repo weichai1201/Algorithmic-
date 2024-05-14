2fc95450b8907435ecf4fb5ce102a9f3523e0576
McNagi <huanjie@protonmail.com>
2024-05-15 09:14:49 +1000

    update to non-decreasing margin

--- src/agent/margins.py
+++ src/agent/margins.py
@@ -9,7 +9,13 @@ class Margins:
         self._margins: List[Tuple[datetime, float]] = list()
 
     def append_margin(self, date: datetime, margin: float):
+        margin = max(margin, self._get_last_margin())
         self._margins.append(tuple((date, margin)))
+
+    def _get_last_margin(self):
+        if len(self._margins) == 0:
+            return 0
+        return self._margins[len(self._margins) - 1][1]
 
     def get_margins(self):
         return self._margins
